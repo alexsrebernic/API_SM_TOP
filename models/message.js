3d0ecb91f98f2b24ffd1692bdcb5f2bf458f501f
Alex Srebernic <alexsrebernic@pop-os.localdomain>
2022-01-18 08:17:06 -0300

    add message model

--- models/message.js
+++ models/message.js
@@ -5,4 +5,6 @@ const messageSchema = new Schema({
     author:{type:Schema.Types.ObjectId, ref:"User",required:true},
     content:{type:String,required:true},
     chat:{type:Schema.Types.ObjectId, ref:"Chat",required:true},
+    date:{type:String,required:true}
 })
+module.exports = mongoose.model('Message', messageSchema);
