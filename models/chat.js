7f93b8a8488c806b4419871b48b4ea0bf380fc8f
Alex Srebernic <alexsrebernic@pop-os.localdomain>
2022-01-18 07:55:50 -0300

    add initial model chat

---
+++ models/chat.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const chatSchema = new Schema({
+    user1:{type:Schema.Types.ObjectId, ref:"User",required:true},
+    user2:{type:Schema.Types.ObjectId, ref:"User",required:true},
+    messages:[{type:Schema.Types.ObjectId, ref:"Messages",required:false}],
+
+
+})
+module.exports = mongoose.model('Chat', chatSchema);
--- models/user.js
+++ models/user.js
@@ -7,10 +7,10 @@ var userSchema = new Schema(
       username: {type: String, required: true, maxLength: 25},
       password: {type:String,required:true},
       details:{type:String,required:false},
-      posts:[{type:Schema.Types.ObjectId,required:false}],
-      friends:[{type:Schema.Types.ObjectId,required:false}],
-      chats:[{type:Schema.Types.ObjectId,required:false}],
-      notifications:[{type:Schema.Types.ObjectId,required:false}]
+      posts:[{type:Schema.Types.ObjectId,required:false,ref:"Post"}],
+      friends:[{type:Schema.Types.ObjectId,required:false,ref:"User"}],
+      chats:[{type:Schema.Types.ObjectId,required:false,ref:"Chat"}],
+      notifications:[{type:Schema.Types.ObjectId,required:false,ref:"Notification"}]
     }
   );
 
