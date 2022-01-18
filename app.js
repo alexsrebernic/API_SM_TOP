90bc1e93f6370fa0987dde5a60f1c451500f4933
Alex Srebernic <alexsrebernic@pop-os.localdomain>
2022-01-18 07:18:20 -0300

    first commit

--- app.js
+++ app.js
@@ -6,10 +6,11 @@ var logger = require('morgan');
 const dotenv = require('dotenv')
 const bodyParser = require("body-parser");
 var compression = require('compression');
-const jwt = require("jsonwebtoken");
 const admin = require('../blog-api/addUserInDB')
 dotenv.config()
 const app = express()
+
+
 // MONGO DB CONNECTION
 var dev_db_url = 'mongodb+srv://cooluser:coolpassword@cluster0-mbdj7.mongodb.net/local_library?retryWrites=true'
 var mongoDB = process.env.MONGODB_URI || dev_db_url;
