307ecc588a5d9d4d829f2ad6b4620ee0906d3a69
Alex Srebernic <alexsrebernic@pop-os.localdomain>
2022-01-21 20:03:12 -0300

    add token header allow

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 const apiRouter = require('./routes/api')
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,token");
   next();
 });
 
