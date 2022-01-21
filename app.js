9639ec021db829a12d7090b9ad1724151afa403c
Alex Srebernic <alexsrebernic@pop-os.localdomain>
2022-01-21 10:26:47 -0300

    add cors headers

--- app.js
+++ app.js
@@ -24,7 +24,11 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 const apiRouter = require('./routes/api')
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(logger('dev'));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }))
