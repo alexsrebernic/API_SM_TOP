27e78e265f133201f3634defac1a42bef6ead680
alexsreberni <alexsrebernic@gmail.com>
2022-01-23 15:32:51 -0300

    edit websocket file

--- utils/websocket/index.js
+++ utils/websocket/index.js
@@ -3,7 +3,7 @@ const io = require('socket.io')(3001,{cors:'localhost:3000'})
 
 io.on("connection",socket => {
     console.log("New user connected with id:",socket.id)
-    socket.on('post:create',(post) => {
+    io.on('post:create',(post) => {
         socket.emit('post:create',post)
         console.log("post emitted")
     })
