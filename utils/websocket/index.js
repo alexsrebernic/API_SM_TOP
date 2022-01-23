4748ef5bb3477ebe262240e28105f4b7ad2fd3c1
alexsreberni <alexsrebernic@gmail.com>
2022-01-22 21:02:36 -0300

    resolve bug path in websocket index

--- utils/websocket/index.js
+++ utils/websocket/index.js
@@ -4,4 +4,4 @@ const io = require('socket.io')(3001,{cors:'localhost:3001'})
 io.on("connection",socket => {
     console.log("New user connected with id:",socket.id)
 })
-exports.module = io
+module.exports = io
