f6d123b12d40800fca5ee0de9f0337a4b15b8939
alexsreberni <alexsrebernic@gmail.com>
2022-01-31 20:47:09 -0300

    fix bugs

---
+++ controllers/chatController.js
@@ -0,0 +1,63 @@
+const io = require('../utils/websocket/index');
+const User = require('../models/user');
+const Notification = require('../models/notification')
+const Chat = require('../models/chat')
+const Message = require('../models/message')
+
+const mongoose = require('mongoose')
+
+
+
+exports.send_message = (req,res,next) => {
+    const {author,user,message,user1,user2} = req.body;
+    User.findById(user1)
+    .populate("chats")
+    .exec((err,user) => {
+        console.log(err)
+        if(user.chats.some(chat => (chat.user1.toString() === user1 || chat.user2.toString() === user1) && (chat.user1.toString() === user2 || chat.user2.toString() === user2))){
+            console.log("send message")
+          const chat = user.chats.find(chat => {return((chat.user1.toString() === user1 || chat.user2.toString() === user1) && (chat.user1.toString() === user2 || chat.user2.toString() === user2))})
+          const newMessage = new Message({
+            author:user1,
+            message,
+            date: new Date
+        }).save((err,message) => {
+            Chat.findByIdAndUpdate(chat._id,{$push:{"messages":message}},{new:true})
+            .populate('messages')
+            .sort({'messages.date':1})
+            .exec((err,chat) => {
+                console.log(err)
+                io.emit('chat:update',chat)
+            })
+        })
+        } else {
+            console.log("create chat")
+            const chat = new Chat({
+                user1,
+                user2,
+            }).save((err,chat) => {
+                io.emit('chat:create',chat)
+                if(err){console.log(err)}
+                User.findByIdAndUpdate(user1,{$push:{"chats":{$each:[chat],$position:0}}},{new:true},(err,user1Updated) => {
+                    if(err){console.log(err)}
+                    const newMessage = new Message({
+                        author:user1,
+                        message,
+                        date: new Date
+                    }).save((err,message) => {
+                        Chat.findByIdAndUpdate(chat._id,{$push:{"messages":message}},{new:true})
+                        .populate('messages')
+                        .exec((err,chat) => {
+                            console.log(err)
+                            io.emit('chat:update',chat)
+                        })
+                    })
+                })
+                User.findByIdAndUpdate(user2,{$push:{"chats":{$each:[chat],$position:0}}},{new:true},(err,user2Updated) => {
+                    if(err){console.log(err)}
+                })
+            })
+        }
+    })
+
+}
---
+++ controllers/notificationController.js
@@ -0,0 +1,91 @@
+const Post = require('../models/post')
+const jwt = require("jsonwebtoken");
+const io = require('../utils/websocket/index')
+const Notification = require('../models/notification');
+const User = require("../models/user")
+exports.notification_update = (req,res,next) => {
+    const id = req.params.id;
+    const {user} = req.body
+    Notification.findByIdAndUpdate(id,{"clicked":true},{new:true},(err,notification) => {
+        console.log(err)
+        if(err){return next(err)}
+        console.log(notification)
+    })
+    User.findById(user)
+    .populate('notifications')
+    .populate('friends')
+    .exec((err,user) => {
+        if(err){
+        }
+        User.populate(user,
+            {path:"notifications.author"}
+            ,(err,userUpdated) => {
+                if(err){return next(err)}
+                console.log(userUpdated)
+                io.emit("user:update",userUpdated)
+            })
+    })
+    
+   
+}
+exports.notification_friend_request_create = (req,res,next) => {
+    const {author,user,} = req.body
+    const notification = new Notification({
+        author,
+        friend_request:true,
+        clicked:false
+    }).save((err,notification) => {
+        if(err){return next(err)}
+        User.findByIdAndUpdate(user,{$push:{"notifications":{$each:[notification._id],$position:0}}},{new:true},(err,user) => {
+            if(err){return next(err)}
+        })
+        User.findByIdAndUpdate(author,{$push:{"friend_requests":{$each:[user],$position:0}}},{new:true})
+        .populate('notifications')
+        .populate('friends')
+        .exec((err,user) => {
+            if(err){
+            console.log(err)
+            }
+            User.populate(user,
+                {path:"notifications.author"}
+                ,(err,userUpdated) => {
+                    if(err){return next(err)}
+                    io.emit("user:update",userUpdated)
+                })
+        })
+    }) 
+
+}
+exports.notification_friend_request_accept = (req,res,next) => {
+    const {author,user,idNotification} = req.body
+    User.findByIdAndUpdate(author,{$pull:{"friends":user}},{new:true},(err,userUpdated) => {
+        console.log(err)
+        User.findByIdAndUpdate(userUpdated,{$push:{"friends":{$each:[user],$position:0}}},{},(err) => {
+            if(err){return next(err)}
+        })
+    })
+    
+    Notification.findByIdAndUpdate(idNotification,{"clicked":true},{new:true},(err,notification) => {
+        if(err){return next(err)}
+    })
+    User.findByIdAndUpdate(user,{$pull:{"friends":author}},{new:true},(err,userUpdated) => {
+        console.log(userUpdated)
+        User.findByIdAndUpdate(userUpdated,{$push:{"friends":{$each:[author],$position:0}}},{new:true})
+            .populate('friends')
+            .populate('notifications')
+            .exec((err,user) => {
+                User.populate(user,
+                    {path:"notifications.author"}
+                    ,(err,userUpdated) => {
+                        if(err){return next(err)}
+                        console.log(userUpdated)
+                        io.emit("user:update",userUpdated)
+                    })
+            
+            })
+        })
+   
+    User.findByIdAndUpdate(author,{$pull:{"friend_requests":user}},{},(err) => {
+        if(err){return next(err)}
+        })
+}
